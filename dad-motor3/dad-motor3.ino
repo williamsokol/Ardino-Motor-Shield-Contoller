9e22913c041964ea37a49556c0187b2c6a79de36
unknown <williamsokol0@gmail.com>
2021-02-06 10:20:31 -0800

    added PID script

--- dad-motor3/dad-motor3.ino
+++ dad-motor3/dad-motor3.ino
@@ -1,3 +1,11 @@
+#define KP 2000
+#define KI  5
+#define KD   100  
+
+#define MOTORMAX 255
+
+#define motorlim(a) (  abs(a) > MOTORMAX) ? ( (a > 0)?MOTORMAX:-MOTORMAX) : (a)
+
 
 #define PWM_A  3
 #define PWM_B 11
@@ -43,48 +51,84 @@ void setup() {
 void loop() {
   
   outputValue = CreateOutput();
- 
-  if(outputValue < DEADSPACE && outputValue > -DEADSPACE){
-    analogWrite(analogOutPin, 0);
+
+
+
+
+  // get direction
+  if(outputValue > 0){
+      digitalWrite(DIR_A, 0);
   }else{
-    analogWrite(analogOutPin, outputValue);
+      digitalWrite(DIR_A, 1);
+    outputValue *= -1;
   }
 
-  // print the results to the Serial Monitor:
-  Serial.print("sensor = ");
-  Serial.print(sensorValue);
-  Serial.print("\t motor = ");
-  Serial.println(motorValue);
+ 
+  if(outputValue < DEADSPACE && outputValue > -DEADSPACE) outputValue = 0;
+  
+  analogWrite(analogOutPin, outputValue);
+
 
   // wait 2 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
-  delay(200);
+  delay(1);
+  
 }
 
 int CreateOutput() {
+
+
+
+
+long int motor =0;
+static long int  intg = 0;
+static int  perr;
+int der;
+
+  
   // read the analog in value:
   sensorValue = analogRead(analogInPin);
   // read motor position
   motorValue = analogRead(analogMotorPin);
-  
+
+
+ long int err = motorValue - sensorValue;
+
+
+    intg += err;
+    der =  err - perr;
+    perr = err;
+
+
+    if(intg > 10000) intg = 10000;  if(intg < 10000) intg = -10000; 
+
   // map it to the range of the analog out:
-  sensorValue = map(sensorValue, 0, 1023, -255, 255);
-  motorValue = map(motorValue, 0, 1023, 255, -255);
+ // motor = map(sensorValue, 0, 1023, -255, 255);
+
+   motor = (err * KP + intg * KI   + (der * KD) )/1000;
+
+
+  // print the results to the Serial Monitor:
+ // Serial.print("s , ");
+  Serial.print(sensorValue);
+//  Serial.print(", m ,");
+Serial.print(" , ");
+  Serial.print(motorValue);
+//  Serial.print(", e ,");
+Serial.print(" , ");
+  Serial.print(err);
+  //  Serial.print(", i ,");
+  Serial.print(" , ");
+  Serial.print(intg);
+ // Serial.print(", d ,");
+ Serial.print(" , ");
+  Serial.print(der);
+//  Serial.print(", m ,");
+Serial.print(" , ");
+  Serial.println(motor);
+
+
+ 
+      return motorlim(motor);  
   
-  // get direction
-  if(sensorValue > 0){
-      digitalWrite(DIR_A, 0);
-  }else{
-      digitalWrite(DIR_A, 1);
-    sensorValue *= -1;
-  }
-  
-  //safety gaurds
-  if(motorValue < MOTIONRANGE || sensorValue < MOTIONRANGE ){
-      return sensorValue;
-  }else if(motorValue > -MOTIONRANGE || sensorValue > -MOTIONRANGE){
-      return sensorValue;
-  }else{
-      return 0;  
-  }
 }
